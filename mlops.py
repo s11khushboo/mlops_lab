6a49c86a892b9ba55a168b6d48363618357f8dd9
s11khushboo <Shrivastava.khushboo00@gmail.com>
2025-11-10 15:32:40 +0100

    lab done

--- mlops.py
+++ mlops.py
@@ -1,3 +1,13 @@
-import torch
-import torch.nn as nn
-from torch.nn import functional as F
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv("FuelConsumptionCo2.csv")
+
+cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+cdf.head(9)
+viz = cdf[['CYLINDERS','ENGINESIZE','CO2EMISSIONS','FUELCONSUMPTION_COMB']]
+viz.hist()
+plt.show()
+
+# take a look at the dataset
+
