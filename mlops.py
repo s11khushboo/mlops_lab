4cb208138ff3b4fd4e6bbcb78f7020f874c393a1
kira vivian <rkira.vivian@gmail.com>
2025-11-10 15:59:42 +0100

    hello

--- mlops.py
+++ mlops.py
@@ -8,6 +8,9 @@ cdf.head(9)
 viz = cdf[['CYLINDERS','ENGINESIZE','CO2EMISSIONS','FUELCONSUMPTION_COMB']]
 viz.hist()
 plt.show()
+print("hello")
 
 # take a look at the dataset
+
+# change to push to github
 
